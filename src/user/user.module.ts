0f6815c92495be234505c749a84933c030f1ce26
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-24 16:31:46 +0530

    Change the class

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { user } from './user.entity';
+import { User } from './User.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([user])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
